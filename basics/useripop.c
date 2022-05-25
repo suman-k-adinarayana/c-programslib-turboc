c732ac17718f81f7906023e1722866975647f46a
Suman K Adinarayana <67361562+suman-k-adinarayana@users.noreply.github.com>
2022-05-25 13:05:01 +0530

    create useripop.c
    .

---
+++ basics/useripop.c
@@ -0,0 +1,11 @@
+#include<stddio.h>
+#include<conio.h>
+void main()
+{
+int var1, var2, var3;
+clrscr();
+printf("Enter 3 Ages\n");
+scanf("%d %d %d",&var1,&var2,&var3);
+printf("Three ages are %d %d %d", var1, var2, var3);
+
+}
